d22b71b9b4c50a4e17791ba4798397e4c6ecb18d
Antonio Calanducci <tcaland@Gin0.local>
2012-02-02 22:59:34 +0100

    prova log

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
+	console.log(req.url);
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World from nodester..');
 }).listen(14236);
+
+console.log("Server avviato\n");
