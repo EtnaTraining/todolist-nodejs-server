2a6bdd3d136ab76c68a509500c85a869355f8ff3
Antonio Calanducci <tcaland@Gin0.local>
2012-02-02 23:45:05 +0100

    json test

--- server.js
+++ server.js
@@ -2,7 +2,22 @@
 var app = require('express').createServer();
    
 app.get('/', function(req, res) {
-     res.send('Hello, world!');
+     res.send('ToDo List test!');
 });
-   
+
+app.get('/movies', function (req, res) {
+      res.contentType('json');
+      res.json({
+          success: true,
+          data: [{
+              title: "The Matrix",
+              year: 1999
+          }, {
+              title: "Star Wars: Return of the Jedi",
+              year: 1983
+          }]
+      });
+  });
+
+
 app.listen(14236);
