7d49f093953935b5a988ca3f8a2804644922f587
Antonio Calanducci <tcaland@Gin0.local>
2012-02-03 00:34:17 +0100

    testing POST

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ var Movie = mongoose.model('movies', new mongoose.Schema({
     year: Number
 }));
 
+app.configure(function () {
+    //app.set('views', __dirname + '/views');
+    //app.set('view engine', 'jade');
+    app.use(express.bodyParser());//parse JSON into objects
+    app.use(express.methodOverride());
+    app.use(app.router);
+    //app.use(express.static(__dirname + '/public'));
+});
+
+
+
    
 app.get('/', function(req, res) {
      res.send('ToDo List test!');
