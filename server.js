fef88b60042dc05883aee5f51b9feab0eb560d3e
Antonio Calanducci <tcaland@Gin0.local>
2012-02-02 22:56:04 +0100

    just updating some title

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World 2\nApp (todolist) is running..');
+  res.end('Hello World from nodester..');
 }).listen(14236);
