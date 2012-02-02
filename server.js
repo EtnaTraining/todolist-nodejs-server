870e784dcd139ad06444875ddd9c9fbd373f3b5e
Antonio Calanducci <tcaland@Gin0.local>
2012-02-02 23:59:22 +0100

    added REST operations

--- server.js
+++ server.js
@@ -24,5 +24,49 @@ app.get('/movies', function (req, res) {
     });
 });
 
+app.put('/movies/:id', function(req, res){
+    movieModel.find({_id: req.params.id}, function (err, movies) {
+        // update db
+        var movie = movies[0];
+        movie.set(req.body);
+        movie.save(function (err) {
+            res.contentType('json');
+            res.json({
+                success: !err,
+                data: req.body
+            });
+        });
+    });
+});
+
+app.post('/movies', function (req, res) {
+    var newMovie = new Movie();
+    var newMovieData = req.body;
+
+    // remove the id which the client sends since it is a new Movie
+    delete newMovieData['_id'];
+    newMovie.set(newMovieData);
+    newMovie.save(function (err, movie) {
+        res.contentType('json');
+        res.json({
+            success: !err,
+            data: movie
+        });
+    });
+});
+
+
+app.del('/movies/:id', function(req, res){
+    Movie.remove({_id: req.params.id}, function (err, movies) {
+        res.contentType('json');
+        res.json({
+            success: !err,
+            data: []
+        });
+    });
+});
+
+
+
 
 app.listen(14236);
