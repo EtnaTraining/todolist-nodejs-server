61cfcbc8609046aca02beb7fe300018bdc63db50
Antonio Calanducci <tcaland@Gin0.local>
2012-02-02 23:53:43 +0100

    added mongo schema

--- server.js
+++ server.js
@@ -2,7 +2,13 @@
 var app = require('express').createServer();
 var mongoose = require('mongoose');
 
-var db = mongoose.connect('mongodb://tcaland:s1stem1@staff.mongohq.com:10010/example'),
+var db = mongoose.connect('mongodb://tcaland:s1stem1@staff.mongohq.com:10010/example');
+
+var movieModel = mongoose.model('movies', new mongoose.Schema({
+    title: String,
+    year: Number
+}));
+
    
 app.get('/', function(req, res) {
      res.send('ToDo List test!');
