88d532a7318326d0f884ffda8f0d539e78e890ff
Antonio Calanducci <info@etnatraining.it>
2012-02-10 11:20:23 +0100

    serving static files

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.configure(function () {
     app.use(express.bodyParser());//parse JSON into objects
     app.use(express.methodOverride());
     app.use(app.router);
-    //app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/public'));
 });
 
 
@@ -68,66 +68,5 @@ app.post('/:udid', function (req, res) {
         });
     });
 });
-
-
-
- 
-
-app.get('/movies', function (req, res) {
-    Movie.find({}, function (err, movies) {
-        res.contentType('json');
-        res.json({
-            success: true,
-            data: movies
-        });
-    });
-});
-
-app.put('/movies/:id', function(req, res){
-    Movie.find({_id: req.params.id}, function (err, movies) {
-        // update db
-        var movie = movies[0];
-        movie.set(req.body);
-        movie.save(function (err) {
-            res.contentType('json');
-            res.json({
-                success: !err,
-                data: req.body
-            });
-        });
-    });
-});
-
-app.post('/movies', function (req, res) {
-	//console.log(req.body);
-    var newMovie = new Movie();
-    var newMovieData = req.body;
-
-    // remove the id which the client sends since it is a new Movie
-    //delete newMovieData['_id'];
-	//console.log(newMovieData);
-    newMovie.set(newMovieData);
-    newMovie.save(function (err, movie) {
-        res.contentType('json');
-        res.json({
-            success: 0,
-            data: movie
-        });
-    });
-});
-
-
-app.del('/movies/:id', function(req, res){
-    Movie.remove({_id: req.params.id}, function (err, movies) {
-        res.contentType('json');
-        res.json({
-            success: !err,
-            data: []
-        });
-    });
-});
-
-
-
 
 app.listen(14236);
